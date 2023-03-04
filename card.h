a94c4cb9d54c427eee362a942e3e6437f63ee364
Viacheslav Myrhorod <slavik.mirgorod@gmail.com>
2023-03-04 19:01:37 +0200

    first commit

---
+++ card.h
@@ -0,0 +1,15 @@
+#ifndef CARD_H
+#define CARD_H
+#include <string>
+
+using namespace std;
+
+class Card {
+private:
+    string name;
+    string suit;
+    int value;
+    string path;
+};
+
+#endif // CARD_H
